27f7cdfe1fa6715167e012246b1cb9b7577f8498
Huy Trần <trantienhuy1112@gmail.com>
2022-10-07 20:52:38 +0700

    mini obj

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="products-catagories-area clearfix">
+      <div className="amado-pro-catagory clearfix">
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/1.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $180</p>
+              <h4>Modern Chair</h4>
+            </div>
+          </a>
+        </div>
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/2.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $180</p>
+              <h4>Minimalistic Plant Pot</h4>
+            </div>
+          </a>
+        </div>
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/3.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $180</p>
+              <h4>Modern Chair</h4>
+            </div>
+          </a>
+        </div>
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/4.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $180</p>
+              <h4>Night Stand</h4>
+            </div>
+          </a>
+        </div>
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/5.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $18</p>
+              <h4>Plant Pot</h4>
+            </div>
+          </a>
+        </div>
+        {/* Single Catagory */}
+        <div className="single-products-catagory clearfix">
+          <a href="shop.html">
+            <img src="img/bg-img/6.jpg" alt="" />
+            {/* Hover Content */}
+            <div className="hover-content">
+              <div className="line" />
+              <p>From $320</p>
+              <h4>Small Table</h4>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
